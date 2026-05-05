e5cdbafd3f11c415eea458025fed81e7ff6c3211
Neeraj <neeraj.cseh@gmail.com>
2026-05-05 12:12:59 +0530

    add

--- Day_2.py
+++ Day_2.py
@@ -38,13 +38,48 @@
 #         second = i
 # print(second)
 
-n = [5, 3, 2, 2, 1, 5, 5, 7, 5, 10]
-m = [10, 111, 1, 9, 5, 67, 2]
-freq = [0] * 11
-for i in n:
-    freq[i] +=1
-for i in m:
-    if i < 0 or i > 10:
-        print(i, "->", 0)
-    else:
-        print(i, "->", freq[i])
+# n = [5, 3, 2, 2, 1, 5, 5, 7, 5, 10]
+# m = [10, 111, 1, 9, 5, 67, 2]
+# freq = [0] * 11
+# for i in n:
+#     freq[i] +=1
+# for i in m:
+#     if i < 0 or i > 10:
+#         print(i, "->", 0)
+#     else:
+#         print(i, "->", freq[i])
+
+# count = 0
+# def fun():
+#     global count
+#     if count == 5:
+#         return
+#     print("Hello")
+#     count += 1
+#     fun()
+# fun()
+
+# count = 0
+# def fun():
+#     global count 
+#     if count == 5:
+#         return
+#     count += 1
+#     fun()
+#     print("Hello")
+# fun()
+
+# def fun(x, n):
+#     if n == 0:
+#         return
+#     print(x)
+#     fun(x, n-1)
+# fun("Hello", 5)
+
+# def fun (x, n):
+#     if x > n:
+#         return
+#     print(x)
+#     fun(x+1, n)
+# fun(1, 5)
+
