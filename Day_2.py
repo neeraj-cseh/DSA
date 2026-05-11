8efe9afac922511bbcf0708c2e0430bdacccfb0c
Neeraj <neeraj.cseh@gmail.com>
2026-05-11 11:36:02 +0530

    add

--- Day_2.py
+++ Day_2.py
@@ -181,3 +181,26 @@
 # bubble_sort(arr)
 # print(arr)
 
+# arr = [3, 5, 6, 4, 8, 9, 10, 7, 1]
+# n = len(arr)
+# for i in range(1,n):
+#     key = arr[i]
+#     j = i - 1
+#     while j >= 0 and arr[j] > key:
+#         arr[j + 1] = arr[j]
+#         j -= 1
+#     arr[j + 1] = key
+# print(arr)
+
+# def insertion_sort(arr):
+#     n = len(arr)
+#     for i in range (1, n):
+#         key = arr[i]
+#         j = i - 1
+#         while j >= 0 and arr[j] > key:
+#             arr[j + 1] = arr[j]
+#             j -= 1
+#         arr[j + 1] = key
+# arr = [3, 5, 6, 4, 8, 9, 10, 7, 1]
+# insertion_sort(arr)
+# print(arr)
